2ee127739f9cf3b5a9b27c92daedcefb963d3052
Mert Ernaz <rnzappdroid@gmail.com>
2023-06-08 23:17:05 +0300

    ATM

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structures
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lutfen yapmak istediğiniz işlemi seçiniz :");
+                int x=Convert.ToInt32(Console.ReadLine());
+            switch(x){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+
+
+            }
+
+        }
+
+        public static enum ATM_Fonks
+        {
+            ParaCek,ParaYatir,BorcOde
+        } 
+            
+        
+    }
+}
